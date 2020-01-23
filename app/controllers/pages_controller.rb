9155815ebe796271815da7d3c0c36fc955ce45cd
anurag <anurag1998mishra@gmail.com>
2020-01-24 01:04:58 +0530

    Refactor the views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,10 +8,11 @@ class PagesController < ApplicationController
 
   def dashboard
     @scheduled = current_user.posts.where(state: 'scheduled')
-                             .paginate(page: params[:scheduled_page], per_page: 4)
+                             .paginate(page: params[:scheduled_page],
+                                       per_page: 2)
                              .order('scheduled_at ASC')
     @history = current_user.posts.where.not(state: 'scheduled')
-                           .paginate(page: params[:history_page], per_page: 4)
+                           .paginate(page: params[:history_page], per_page: 2)
                            .order('scheduled_at DESC')
   end
 end
